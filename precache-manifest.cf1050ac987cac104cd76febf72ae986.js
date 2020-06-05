27fca1851c7286254965d5653830489d89b39aff
erishdaren <moreno240190@gmail.com>
2020-06-05 14:44:56 -0500

    Updates

---
+++ precache-manifest.cf1050ac987cac104cd76febf72ae986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00aa23de74e90d2c838759cf8ae21abd",
+    "url": "/Pantallas-VICOSERTRA/index.html"
+  },
+  {
+    "revision": "43d828f3b5f8d88be40e",
+    "url": "/Pantallas-VICOSERTRA/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82552a800f42095a0071",
+    "url": "/Pantallas-VICOSERTRA/static/js/2.fab85eac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Pantallas-VICOSERTRA/static/js/2.fab85eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d828f3b5f8d88be40e",
+    "url": "/Pantallas-VICOSERTRA/static/js/main.1b541624.chunk.js"
+  },
+  {
+    "revision": "052277bf83c422833536",
+    "url": "/Pantallas-VICOSERTRA/static/js/runtime-main.6dd39601.js"
+  }
+]);
